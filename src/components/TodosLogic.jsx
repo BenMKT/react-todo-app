78858c965fe58ead30752aff1aa8a7f352bf3b38
Benson Kiai <benson.kiai86@gmail.com>
2023-08-15 19:46:34 +0300

    Refactor delTodo function code block to work

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -22,7 +22,11 @@ const TodosLogic = () => {
   ]);
 
   const delTodo = (id) => {
-    console.log('deleted', id);
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
   };
 
   return (
