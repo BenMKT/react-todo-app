49951c99151686931ef2cb3278a29d94a87b3d3e
Benson Kiai <benson.kiai86@gmail.com>
2023-08-15 20:34:25 +0300

    Use drilled addTodoItem prop

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,20 +1,26 @@
 import { useState } from 'react';
 
-const InputTodo = () => {
+const InputTodo = ({ addTodoItem }) => {
   const [title, setTitle] = useState('');
 
   const handleChange = (e) => {
     setTitle(e.target.value);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    addTodoItem(title);
     setTitle('');
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" value={title} onChange={handleChange} placeholder="Add Todo..." />
+      <input
+        type="text"
+        value={title}
+        onChange={handleChange}
+        placeholder="Add Todo..."
+      />
       <button>Submit</button>
     </form>
   );
