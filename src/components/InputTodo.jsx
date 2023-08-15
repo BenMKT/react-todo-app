b26aa876976d509ccd4636349a62034d673926a3
Benson Kiai <benson.kiai86@gmail.com>
2023-08-15 20:06:39 +0300

    Control the Text Input state onChange

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+
 const InputTodo = () => {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  }
+
   return (
     <form>
-      <input type="text" placeholder="Add Todo..." />
+      <input type="text" value={title} onChange={handleChange} placeholder="Add Todo..." />
       <button>Submit</button>
     </form>
   );
