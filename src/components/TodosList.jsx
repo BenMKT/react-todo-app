d2e089784bd72b3fe5bebb1ecf6aed789344044b
Benson Kiai <benson.kiai86@gmail.com>
2023-08-15 19:36:04 +0300

    delTodo prop drilling

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,10 +1,15 @@
 import TodoItem from './TodoItem';
 
-const TodosList = ({ todosProps, setTodos }) => {
+const TodosList = ({ todosProps, setTodos, delTodo }) => {
   return (
     <ul>
       {todosProps.map((todo) => (
-        <TodoItem key={todo.id} itemProp={todo} setTodos={setTodos} />
+        <TodoItem
+          key={todo.id}
+          itemProp={todo}
+          setTodos={setTodos}
+          delTodo={delTodo}
+        />
       ))}
     </ul>
   );
