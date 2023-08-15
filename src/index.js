76e8c3bf7c83bb857805e623a7bbdc278c333b96
Benson Kiai <benson.kiai86@gmail.com>
2023-08-16 00:38:24 +0300

    Import styles from respective scss component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './styles/app.scss';
 import TodoApp from 'components/TodoApp';
 
 const domContainer = document.getElementById('root');
