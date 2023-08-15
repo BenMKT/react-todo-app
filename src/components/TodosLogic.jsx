b96cf60d119c5965a96426e77f5d02bd4bb64d8f
Benson Kiai <benson.kiai86@gmail.com>
2023-08-15 19:10:02 +0300

    Import and initialize todos state

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,31 +1,32 @@
+import { useState } from 'react';
 import InputTodo from './InputTodo';
 import TodosList from './TodosList';
 
 const TodosLogic = () => {
-    const todos = [
-      {
-        id: 1,
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: 2,
-        title: 'Develop website and add content',
-        completed: false,
-      },
-      {
-        id: 3,
-        title: 'Deploy to live server',
-        completed: false,
-      },
-    ];
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ]);
 
-    return (
-        <div>
-            <InputTodo />
-            <TodosList todosProps={todos} />
-        </div>
-    );
-}
+  return (
+    <div>
+      <InputTodo />
+      <TodosList todosProps={todos} setTodos={setTodos} />
+    </div>
+  );
+};
 
 export default TodosLogic;
