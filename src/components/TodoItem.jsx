dfefdb7b2652bc6044c122a220338caf701703f1
Benson Kiai <benson.kiai86@gmail.com>
2023-08-16 00:51:30 +0300

    Add classes and Import sass with some dynamic inline styles

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,3 +1,5 @@
+import '../styles/todoItem.scss';
+
 const TodoItem = ({ itemProp, setTodos, delTodo }) => {
   const handleChange = (id) => {
     setTodos((prevState) =>
@@ -12,15 +14,27 @@ const TodoItem = ({ itemProp, setTodos, delTodo }) => {
       })
     );
   };
+
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
   return (
-    <li>
-      <input
-        type="checkbox"
-        checked={itemProp.completed}
-        onChange={() => handleChange(itemProp.id)}
-      />
-      <button onClick={() => delTodo(itemProp.id)}>Delete</button>
-      {itemProp.title}
+    <li className="item">
+      <div className="content">
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+        <span style={itemProp.completed ? completedStyle : null}>
+          {itemProp.title}
+        </span>
+      </div>
     </li>
   );
 };
